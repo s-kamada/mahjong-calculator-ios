6004cd2712d0a9e4d365729557c8e26d68d86aca
Shunsuke Kamada <mod.rab7219@gmail.com>
2021-04-02 01:58:02 +0900

    ボタン、入力欄追加

--- mahjong.calculator/ViewController.swift
+++ mahjong.calculator/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fanTextField: UITextField!
+    @IBOutlet weak var fuTextField: UITextField!
+    @IBOutlet weak var calculateButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         print("view did load")
