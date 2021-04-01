c36dbece085775034fb4d2ee7b9107d37faee206
Shunsuke Kamada <mod.rab7219@gmail.com>
2021-04-02 01:00:54 +0900

    Add initial files

---
+++ mahjong.calculator/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  mahjong.calculator
+//
+//  Created by 鎌田峻輔 on 2021/03/31.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ mahjong.calculator/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  mahjong.calculator
+//
+//  Created by 鎌田峻輔 on 2021/03/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("view did load")
+    }
+}
+
