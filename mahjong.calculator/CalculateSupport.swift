0664cf4fe996f96e5b99eb36e349500c1b459dbe
Shunsuke Kamada <mod.rab7219@gmail.com>
2021-04-03 03:14:57 +0900

    点数計算

--- mahjong.calculator/CalculateSupport.swift
+++ mahjong.calculator/CalculateSupport.swift
@@ -15,8 +15,30 @@ final class CalculateSupprt {
      */
     // TODO: 点数計算の条件に関わるものをtypealiasとかでまとめる
     // 親子・飜・符・ツモロン
-    class func score(fan: Int, fu: Int) -> Int {
+    static func score(fan: Int, fu: Int, position: Position) -> Int {
 
+        switch fan {
+        case 1,2,3,4:
+            return calculatescore(fan: fan, fu: fu)
+
+        case 5:
+            return ScoreConstants(as: position).mangan
+
+        case 6,7:
+            return ScoreConstants(as: position).haneman
+
+        case 8,9,10:
+            return ScoreConstants(as: position).baiman
+
+        case 11,12:
+            return ScoreConstants(as: position).sanbaiman
+
+        default:
+            return ScoreConstants(as: position).yakuman
+        }
+    }
+
+    private static func calculatescore(fan: Int, fu: Int) -> Int {
         let score: Double = Double(fu * 4 * Int(pow(Double(2), Double(fan + 2)))) / 100
         let ceiledScore = ceil(score) * 100
 
