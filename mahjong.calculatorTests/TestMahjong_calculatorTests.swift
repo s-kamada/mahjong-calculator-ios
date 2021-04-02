3359585543314aae86cafeb879fefd641190ae34
Shunsuke Kamada <mod.rab7219@gmail.com>
2021-04-02 10:46:15 +0900

    命名変更

--- mahjong.calculatorTests/TestMahjong_calculatorTests.swift
+++ mahjong.calculatorTests/TestMahjong_calculatorTests.swift
@@ -6,8 +6,9 @@
 //
 
 import XCTest
+@testable import mahjong_calculator
 
-class mahjong_calculatorTests: XCTestCase {
+class TestMahjong_calculatorTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
